6a7fe470357db53cde0873bc151fefd00276dbd5
RodiaGiordano <rodiagiordano93@gmail.com>
2024-11-30 13:49:12 +0100

    add relation in Event

---
+++ app/Models/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'name',
+        'active'
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function directory(){
+        return $this->hasMany(Directory::class);
+    }
+}
