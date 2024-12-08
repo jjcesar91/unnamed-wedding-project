3ebc0b47fb035c58777e3d8ac55d0df9a0d32aa9
RodiaGiordano <rodiagiordano93@gmail.com>
2024-12-08 16:34:10 +0100

    add random role in user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -25,6 +26,7 @@ class UserFactory extends Factory
      */
 
     protected static $index = 0;
+    protected static $roles = ['admin','owner','guest'];
 
     public function definition(): array
     {
@@ -32,6 +34,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => 'admin-' . ++self::$index . '@email.it',
+            'role' => Arr::random(static::$roles),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
