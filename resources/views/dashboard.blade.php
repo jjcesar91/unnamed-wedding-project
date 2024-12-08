b4b19a81eeb36a11b3d3babcbcb65a85acd0da11
RodiaGiordano <rodiagiordano93@gmail.com>
2024-12-08 21:41:35 +0100

    add first layout for index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <a href="{{route("events.create")}}" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Vai alal crezione evento</a>
+        <a href="{{route("events.index")}}" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">I tuoi eventi</a>
+
+        <a href="{{route("events.create")}}" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Crea un evento</a>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
