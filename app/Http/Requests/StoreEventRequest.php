afa6ee7e3aa3ac335fc03ed402354e156435bb66
RodiaGiordano <rodiagiordano93@gmail.com>
2024-12-08 16:57:27 +0100

    add rules and messages for validation storeEvent

--- app/Http/Requests/StoreEventRequest.php
+++ app/Http/Requests/StoreEventRequest.php
@@ -11,7 +11,8 @@ class StoreEventRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        if(auth()->check() && auth()->user()->role === 'admin') return true;
+        abort(403, 'Accesso negato. Non hai i permessi per creare un Evento.');
     }
 
     /**
@@ -22,7 +23,39 @@ class StoreEventRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "title" => "required|string|max:20|",
+            "img" => "string|max:20",
+            "description" => "string|max:255";
+            'date' => 'date|after_or_equal:today',
+            "location"=> "required|string|max:40",
+            'type' => 'required|in:matrimonio,promessa,battesimo,cresima,argento,oro,platino,compleanno,rinnovo,baby',
+            "active"=>"boolean"
+        ];
+    }
+
+    public functon message(): array{
+        return [
+            'title.required' => 'Il titolo è obbligatorio.',
+            'title.string' => 'Il titolo deve essere una stringa.',
+            'title.max' => 'Il titolo non può essere più lungo di 20 caratteri.',
+            
+            'img.string' => 'L\'immagine deve essere una stringa.',
+            'img.max' => 'L\'immagine non può essere più lunga di 20 caratteri.',
+            
+            'description.string' => 'La descrizione deve essere una stringa.',
+            'description.max' => 'La descrizione non può essere più lunga di 255 caratteri.',
+            
+            'date.date' => 'La data deve essere una data valida.',
+            'date.after_or_equal' => 'La data deve essere uguale o successiva a oggi.',
+            
+            'location.required' => 'La località è obbligatoria.',
+            'location.string' => 'La località deve essere una stringa.',
+            'location.max' => 'La località non può essere più lunga di 40 caratteri.',
+            
+            'type.required' => 'Il tipo è obbligatorio.',
+            'type.in' => 'Il tipo deve essere uno dei seguenti: matrimonio, promessa, battesimo, cresima, argento, oro, platino, compleanno, rinnovo, baby.',
+            
+            'active.boolean' => 'Lo stato attivo deve essere vero o falso.',
         ];
     }
 }
